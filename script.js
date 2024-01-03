1c029c4eabf603dec303e49029fb07c57df6dc0f
creyes25 <carolina.reyes2022@gmail.com>
2024-01-03 04:17:58 -0600

    add an object to be able to convert num to words

--- script.js
+++ script.js
@@ -9,7 +9,36 @@ let num1, num2, operatorUsed
 let operatorInUse = false
 let equalHasBeenUsed = false
 
-
+const numToWords = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+}
 
 numbers.forEach(num => {
   num.addEventListener('click', (e) => {
@@ -54,7 +83,6 @@ function gettingOperator (e) {
     operatorUsed = '+'
   }
 
-  // const operatorWasChanged = 
   const lastStringOfEquation = equationDisplay.innerHTML[equationDisplay.innerHTML.length - 1]
 
   equationDisplay.innerHTML += operatorUsed
